1977dadc049bf7d4a638923059abb29b7b2f08f8
Kaio Cesar <kadoozin@gmail.com>
2025-06-24 21:30:18 -0300

    feat: Atualiza o controller com mock de dados.

--- src/main/java/br/com/kadoozin/cadastro_de_ninjas/Ninjas/controller/NinjaController.java
+++ src/main/java/br/com/kadoozin/cadastro_de_ninjas/Ninjas/controller/NinjaController.java
@@ -1,14 +1,38 @@
 package br.com.kadoozin.cadastro_de_ninjas.Ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
-    @GetMapping("/ninja")
-    public String ninja(){
-       return "Ninja Controller";
+    // Adiciona um novo ninja (CREATE)
+    @PostMapping("/add")
+    public String criarNinja(){
+        return "..";
     }
+
+    // Mostrar o ninja por ID (READ)
+    @GetMapping("/id")
+    public String mostrarNinjaPorId(){
+        return "..";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/all")
+    public String mostrarTodosOsNinjas(){
+        return "..";
+    }
+
+    // Altera os dados do ninja (UPDATE)
+    @PutMapping("/update")
+    public String updateNinja(){
+        return "..";
+    }
+
+    // Deleta o ninja por ID (DELETE)
+    @DeleteMapping("/deleteId")
+    public String deletarNinja(){
+        return "..";
+    }
+
 }
