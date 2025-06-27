56810a130a74586c23b30267d5216a94a87787b3
Kaio Cesar <kadoozin@gmail.com>
2025-06-26 22:26:16 -0300

    fix: Cria as rotas do controller de missoes com mock

--- src/main/java/br/com/kadoozin/cadastro_de_ninjas/Missoes/controller/MissoesController.java
+++ src/main/java/br/com/kadoozin/cadastro_de_ninjas/Missoes/controller/MissoesController.java
@@ -1,4 +1,31 @@
 package br.com.kadoozin.cadastro_de_ninjas.Missoes.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+    @PostMapping("/add")
+    public String adicionarMissoes(){
+        return "Missão adicionada: ";
+    }
+    @GetMapping("/id")
+    public String missoesId(){
+        return "ID da missão: ";
+    }
+
+    @GetMapping("/all")
+    public String mostrarTodasAsMissoes(){
+        return "Todas as missões: ";
+    }
+
+    @PutMapping("/update")
+    public String updateMissoes(){
+        return "Missão Atualizada: ";
+    }
+
+    @DeleteMapping("/delete")
+    public String deletarMissao(){
+        return "Missão deletada: ";
+    }
 }
